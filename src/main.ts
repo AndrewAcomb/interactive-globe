03fc63a841ba8a1e6d05598db2367f311343a097
Andrew Acomb <acombandrew@gmail.com>
2023-11-23 11:37:30 -0500

    Fix minimum zoom, try to improve image quality

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-// Basic scene setup
+const SPHERE_RADIUS = 5
+const SPHERE_WIDTH_SEGMENTS = 128
+const SPHERE_HEIGHT_SEGMENTS = 128
+const CAMERA_HEIGHT = 20
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -9,26 +13,38 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-// Load Earth texture map
 const textureLoader = new THREE.TextureLoader()
 const earthTexture = textureLoader.load('../world.jpg')
+// const earthTexture = textureLoader.load('../world.svg', (texture) => {
+//   texture.wrapS = THREE.ClampToEdgeWrapping;
+//   texture.wrapT = THREE.ClampToEdgeWrapping;
+//   texture.minFilter = THREE.LinearFilter;
+//   texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+//   renderer.render(scene, camera);
+// });
 
-// Sphere creation
-const geometry = new THREE.SphereGeometry(5, 32, 32)
-// Scale the Y-axis to create an oblate spheroid shape
-// The Earth's polar radius is about 98.3% of its equatorial radius
-geometry.scale(1, 0.983, 1)
-
+const geometry = new THREE.SphereGeometry(
+  SPHERE_RADIUS,
+  SPHERE_WIDTH_SEGMENTS,
+  SPHERE_HEIGHT_SEGMENTS
+)
+geometry.scale(1, 0.983, 1) // Scale sphere into an oblate spheroid
 const material = new THREE.MeshBasicMaterial({ map: earthTexture })
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
 
-// Plane creation
-const planeGeometry = new THREE.PlaneGeometry(12, 12, 32, 32)
+const planeGeometry = new THREE.PlaneGeometry(
+  2 * SPHERE_RADIUS,
+  2 * SPHERE_RADIUS,
+  SPHERE_WIDTH_SEGMENTS,
+  SPHERE_HEIGHT_SEGMENTS
+)
 const planeMaterial = new THREE.MeshBasicMaterial({
   color: 0xffffff,
   side: THREE.DoubleSide,
@@ -36,56 +52,39 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 })
 const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(plane)
-
 plane.rotation.x = -Math.PI / 2
 plane.receiveShadow = true
-
 scene.add(plane)
-scene.add(new THREE.AmbientLight(0xffffff, 0.5))
 
+scene.add(new THREE.AmbientLight(0xffffff, 0.5))
 let directionalLight = new THREE.DirectionalLight(0xffffff, 1)
 directionalLight.castShadow = true
 directionalLight.position.set(2, 2, 2)
 scene.add(directionalLight)
 
-// Camera position
-camera.position.z = 20
-
-// Initialize OrbitControls
+camera.position.z = CAMERA_HEIGHT
 const controls = new OrbitControls(camera, renderer.domElement)
-
+controls.minDistance = SPHERE_RADIUS + 1
 const raycaster = new THREE.Raycaster()
 const mouse = new THREE.Vector2()
 
 function onMouseClick(event: MouseEvent) {
   // Calculate mouse position in normalized device coordinates
-  // (-1 to +1) for both components
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-
-  // Update the picking ray with the camera and mouse position
   raycaster.setFromCamera(mouse, camera)
-
-  // Calculate objects intersecting the picking ray
   const intersects = raycaster.intersectObject(sphere)
-
   if (intersects.length > 0) {
-    // Check if the intersection point is within your button area
-    // This is where you define the logic to determine if the clicked area is a button
-    // For example, checking the intersection point's coordinates
     console.log('Button on the sphere clicked')
   }
 }
-
 window.addEventListener('click', onMouseClick)
 
 // Animation loop
 function animate() {
   requestAnimationFrame(animate)
-
   // Required if controls.enableDamping or controls.autoRotate are set to true
   controls.update()
-
   renderer.render(scene, camera)
 }
 
